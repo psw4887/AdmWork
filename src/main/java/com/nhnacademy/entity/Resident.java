0f5285a99202838f738210d640f58caf06efdacb
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-05-30 10:02:32 +0900

    필요없는주석삭제

--- src/main/java/com/nhnacademy/entity/Resident.java
+++ src/main/java/com/nhnacademy/entity/Resident.java
@@ -1,15 +1,12 @@
 package com.nhnacademy.entity;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
